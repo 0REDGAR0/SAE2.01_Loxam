a22ead36fa48125d4c7ec416f34d369d1de6f8ce
Denis-Hodzic-P <denishodzic51@gmail.com>
2025-06-12 23:45:33 +0200

    Reparation presque fini

---
+++ Reparation/UserControls/UCReparation.xaml.cs
@@ -0,0 +1,59 @@
+﻿using SAE2._01_Loxam.Classe.Reservation;
+using SAE2._01_Loxam.Reparation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SAE2._01_Loxam.Classe.Materiel.UserControls
+{
+    /// <summary>
+    /// Logique d'interaction pour UCReparation.xaml
+    /// </summary>
+    public partial class UCReparation : UserControl
+    {
+        public UCReparation()
+        {
+            InitializeComponent();
+            ChargerReparations();
+        }
+
+        private void ChargerReparations()
+        {
+            ReservationDAO reservationDAO = new ReservationDAO();
+            DataGridReparation.ItemsSource = reservationDAO.GetReservationsAffichage();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            CollectionViewSource.GetDefaultView(DataGridReparation.ItemsSource).Refresh();
+        }
+
+        private void DataGridResa_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            throw new NotImplementedException();  
+        }
+
+        private bool RechercheMotClefResa(object obj)
+        {
+            if (obj is ReparationAffichage reparation)
+            {
+                string texteRecherche = txtRecherche.Text?.ToLower() ?? "";
+
+                return reparation.NomMateriel.ToLower().Contains(texteRecherche)
+                    || reparation.NumMateriel.Equals(int.Parse(texteRecherche));
+            }
+            return false;
+        }
+    }
+}
