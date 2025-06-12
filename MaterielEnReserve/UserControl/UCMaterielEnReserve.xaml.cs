7e97f6a2809a1ffcda635be75a1a02037be8f8a6
Edgar BEDU-NEYRINCK <beduneye@iut-acy.local>
2025-06-12 16:41:06 +0200

    Changements mineurs

--- MaterielEnReserve/UserControl/UCMaterielEnReserve.xaml.cs
+++ MaterielEnReserve/UserControl/UCMaterielEnReserve.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SAE2._01_Loxam.Classe.Reservation;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -22,7 +23,24 @@ namespace SAE2._01_Loxam.FicheClients.UserControls
     {
         public UCMaterielEnReserve()
         {
-     
+            InitializeComponent();
+            DataGridMateriel.Items.Filter = RechercheMotClefMateriel;
+        }
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            CollectionViewSource.GetDefaultView(DataGridMateriel.ItemsSource).Refresh();
+        }
+
+        private bool RechercheMotClefMateriel(object obj)
+        {
+            if (obj is ReservationAffichage reservation)
+            {
+                string texteRecherche = txtRecherche.Text?.ToLower() ?? "";
+
+                return reservation.Client.ToLower().Contains(texteRecherche)
+                    || reservation.Materiel.ToLower().Contains(texteRecherche);
+            }
+            return false;
         }
     }
 }
