e5a714a16228aaf7df5640ba5380dbc281509a25
Edgar BEDU-NEYRINCK <beduneye@iut-acy.local>
2025-06-12 15:18:48 +0200

    Ajout de boutons sur les details dont le bouton "retourné" qui est fonctionnel(puis ajout du fichier de BDD pour plus de simplicité)

---
+++ Classe/Materiel/Materiel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SAE2._01_Loxam.Classe.Materiel
+{
+    public class Materiel
+    {
+        public int NumMateriel { get; set; }
+        public int NumEtat { get; set; }
+        public int NumType { get; set; }
+        public string Reference { get; set; }
+        public string NomMateriel { get; set; }
+        public string Descriptif { get; set; }
+        public decimal PrixJournee { get; set; }
+    }
+}
--- EffectuerReservation/DetailReservationWindow.xaml.cs
+++ EffectuerReservation/DetailReservationWindow.xaml.cs
@@ -5,10 +5,53 @@ namespace SAE2._01_Loxam.FicheClients.UserControls
 {
     public partial class DetailReservationWindow : Window
     {
+        private ReservationAffichage reservationCourante;
+        private DataAccess dataAccess = DataAccess.Instance;
+
+        // Remplace ici par l’ID exact de l’état "Réparation" dans ta table ETAT
+        private const int idEtatReparation = 3;
+
         public DetailReservationWindow(ReservationAffichage reservation)
         {
             InitializeComponent();
-            DataContext = reservation;
+            reservationCourante = reservation;
+            DataContext = reservationCourante;
         }
+        private void butOK_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void butRetourne_Click(object sender, RoutedEventArgs e)
+        {
+            var reservationToUpdate = new Reservation
+            {
+                NumReservation = reservationCourante.NumeroReservation,
+                DateRetourReelleLocation = DateTime.Today
+            };
+
+            dataAccess.MettreAJourReservation(reservationToUpdate);
+            MessageBox.Show("Matériel retourné aujourd'hui.");
+            this.Close();
+        }
+
+        private void butReparation_Click(object sender, RoutedEventArgs e)
+        {
+            var materiel = dataAccess.GetMaterielById(reservationCourante.NumeroReservation);
+
+            if (materiel != null)
+            {
+                materiel.NumEtat = idEtatReparation;
+                dataAccess.MettreAJourMateriel(materiel);
+                MessageBox.Show("Matériel envoyé en réparation.");
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("Matériel non trouvé !");
+            }
+        }
+
+
     }
 }
